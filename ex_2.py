0d2f79a982a9268a5a8ce51f7a92d99558826d8e
Marek Zatwarnicki <73995@ue.poznan.pl>
2023-09-28 22:22:13 +0200

    Dodany kolo

--- ex_2.py
+++ ex_2.py
@@ -14,3 +14,9 @@ print("Obwod trojkata wynosi " + str(obwod_trojkat) + ",  pole wynosi " + str(po
 obwod_prostokat = 2*Bok_a+2*Bok_b
 pole_prostokat = Bok_a * Bok_b
 print("Obwod prostokąta wynosi " + str(obwod_prostokat) + ", zas pole wynosi " + str(pole_prostokat) + ".")
+
+Promien_r = 10;
+
+obwod_kolo = 2*math.pi*Promien_r
+pole_kolo = math.pi*Promien_r**2
+print("Obwod koła wynosi " + str(obwod_kolo) + ", zas pole wynisi " + str(pole_kolo) + ".")
