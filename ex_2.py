4fb2858acfcaf0cc920719c7c063688f0ac817b7
Marek Zatwarnicki <73995@ue.poznan.pl>
2023-09-28 22:21:42 +0200

    Dodany prostokat

--- ex_2.py
+++ ex_2.py
@@ -9,3 +9,8 @@ obwod_trojkat = Bok_a + Bok_b + Bok_c
 pole_trojkat = int((Wysokosc_h * Bok_a) / 2)
 
 print("Obwod trojkata wynosi " + str(obwod_trojkat) + ",  pole wynosi " + str(pole_trojkat) + ".")
+
+
+obwod_prostokat = 2*Bok_a+2*Bok_b
+pole_prostokat = Bok_a * Bok_b
+print("Obwod prostokąta wynosi " + str(obwod_prostokat) + ", zas pole wynosi " + str(pole_prostokat) + ".")
