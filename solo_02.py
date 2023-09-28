9db67b1ff884fd1a36a2f31f2083c37304b07332
Marek Zatwarnicki <73995@ue.poznan.pl>
2023-09-28 22:30:30 +0200

    commit solo_02

--- solo_02.py
+++ solo_02.py
@@ -12,20 +12,20 @@ print("Obwod trojkata wynosi " + str(obwod_trojkat) + ",  pole wynosi " + str(po
 
 obwod_prostokat = 2*Bok_a+2*Bok_b
 pole_prostokat = Bok_a * Bok_b
-print("Obwod prostokąta wynosi " + str(obwod_prostokat) + ", zas pole wynosi " + str(pole_prostokat) + ".")
+print("Obwod prostokąta wynosi " + str(obwod_prostokat) + ", pole wynosi " + str(pole_prostokat) + ".")
 
 Promien_r = 10;
 
 obwod_kolo = 2*math.pi*Promien_r
 pole_kolo = math.pi*Promien_r**2
-print("Obwod koła wynosi " + str(obwod_kolo) + ", zas pole wynisi " + str(pole_kolo) + ".")
+print("Obwod koła wynosi " + str(obwod_kolo) + ", pole wynisi " + str(pole_kolo) + ".")
 
 
 d= 10
 obwod_trapez = Bok_a+Bok_b+Bok_c+d
 pole_trapez = ((Bok_a+Bok_b)*Wysokosc_h)/2
-print("Obwod trapezu wynosi " + str(obwod_trapez) + ", zas pole wynisi " + str(pole_trapez) + ".")
+print("Obwod trapezu wynosi " + str(obwod_trapez) + ", pole wynisi " + str(pole_trapez) + ".")
 
 obwod_romb = 2*Bok_a+2*Bok_b
 pole_romb = Bok_a*Bok_b
-print("Obwod romb wynosi " + str(obwod_romb) + ", zas pole wynisi " + str(pole_romb) + ".")
+print("Obwod romb wynosi " + str(obwod_romb) + ", pole wynisi " + str(pole_romb) + ".")
